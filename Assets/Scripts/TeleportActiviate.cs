2775cb180e043d1748879ab6e57ec75f391ee52c
ad3nardi <ad3nardi@gmail.com>
2022-06-15 14:36:59 +1000

    Teleportation

--- Assets/Scripts/TeleportActiviate.cs
+++ Assets/Scripts/TeleportActiviate.cs
@@ -1,30 +1,32 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class TeleportActiviate : MonoBehaviour
 {
-
+    
     //Access the GO containing the teleport con script
     [SerializeField] public GameObject teleCon;
 
     // Ref to input action with the button map
     [SerializeField] public InputActionReference teleActRef;
 
-    [SerializeField] public Event onTeleportActivate;
-    [SerializeField] public Event onTeleportCancel;
+    [Header("Teleport Events")]
+    [SerializeField] public UnityEvent onTeleportActivate;
+    [SerializeField] public UnityEvent onTeleportCancel;
 
     void Start()
     {
         // An Interaction with the teleportActivationReference has been completed and performs a callback to the TeleportModeActivate.
-    //    teleActRef.action.performed += TeleportModeActivate;
+        teleActRef.action.performed += TeleportModeActivate;
         // An Interaction with the teleportActivationReference has been cancelled and performs a callback to the TeleportModeCancel.
         teleActRef.action.canceled += TeleportModeCancel;
     }
 
     // This will let us call a series of events created in the onTeleportActivate events in the inspector
-    //private void TeleportModeActivate(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
+    private void TeleportModeActivate(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
     
     // This will delay the call of the DelayTeleportation function for 0.1 of a second
     private void TeleportModeCancel(InputAction.CallbackContext obj) => Invoke("DelayTeleportation ", .1f);
+
 }
