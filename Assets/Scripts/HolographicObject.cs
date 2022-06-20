2fdbfcd0bca6be6353c75ea4ca72119ef32b4d27
Blake Peck <s210561@students.aie.edu.au>
2022-06-20 11:06:07 +1000

    Hologram ship position equal to the movement of the actual spaceship

--- Assets/Scripts/HolographicObject.cs
+++ Assets/Scripts/HolographicObject.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class HolographicObject : MonoBehaviour
 {
     public Transform attachedObject;
-   
+
+    Vector3 scaleChange;
+    Vector3 offSet;
+    float differnceInScale = 0.05f;
+
+    private void Start()
+    {
+        scaleChange = new Vector3(0.01f, 0.01f, 0.01f);
+        offSet = new Vector3(0.0f, 0.5f, 0.0f);
+    }
+
     // Update is called once per frame
     void Update()
     {
         //Update my position to match the ship's position
-        transform.SetPositionAndRotation(attachedObject.position * 0.01f, attachedObject.rotation);
+        //transform.SetPositionAndRotation(attachedObject.position, attachedObject.rotation);
+        transform.position = attachedObject.position * differnceInScale + offSet;
+        transform.localScale = scaleChange;
+
+        //Hologram.position = Enitty.Position * differenceInScale ;
     }
 }
