03aa7f55ceff44bb30f1c79d79061574e4f53f21
Blake Peck <s210561@students.aie.edu.au>
2022-06-20 16:03:18 +1000

    Wave Timer

---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave
+{
+    float duration = 60.0f;
+    float currentFrame = 0.0f;
+
+    public float UpdateAmount(float amount)
+    {
+        currentFrame += amount;
+        if (currentFrame >= duration)
+        {
+            currentFrame = 0.0f;
+        }
+
+        return currentFrame;
+    }
+}
