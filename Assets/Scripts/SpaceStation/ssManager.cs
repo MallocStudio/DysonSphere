feff7fd209c69d09a1d75a802424d9f35f2fafe2
AD3Nardi <ad3nardi@gmail.com>
2022-06-21 00:09:08 +1000

    physicsButton

--- Assets/Scripts/SpaceStation/ssManager.cs
+++ Assets/Scripts/SpaceStation/ssManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class ssManager : MonoBehaviour
 {
@@ -12,48 +13,125 @@ public class ssManager : MonoBehaviour
 
     [Header ("Plugins")]
     [SerializeField] private Transform combatRing;
-    [SerializeField] private Transform aimRet;
+    [SerializeField] private Transform cannonAimRet;
+    [SerializeField] private Transform mgunAimRet;
+    [SerializeField] private Transform missileAimRet;
     [SerializeField] private List<Transform> weaponList = new List<Transform>();
 
-    [SerializeField] private int curBay;
+    [Header("Combat Stats")]
     [SerializeField] private float rotateSpd;
+    [SerializeField] private float cannonFireRate;
+    [SerializeField] private float mgunFireRate;
+    [SerializeField] private float cannonReload;
+    [SerializeField] private float mgunReload;
+    [SerializeField] private float missileReload;
 
     [Header("Station Status")]
-    [SerializeField] private bool gunneryMode;
+    [SerializeField] private weaponBay curBay;
+    [SerializeField] public bool gunneryMode;
+    [SerializeField] private float cannonTimer;
+    [SerializeField] private float mgunTimer;
+    [SerializeField] private float missileTimer;
+    
+    [Header("Station Status GUI")]
+    [SerializeField] private GameObject cannonRet;
+    [SerializeField] private GameObject mgunRet;
+    [SerializeField] private GameObject missileRet;
+    [SerializeField] private TMP_Text TMPcannonTimer;
+    [SerializeField] private TMP_Text TMPmgunTimer;
+    [SerializeField] private TMP_Text TMPmissileTimer;
 
+
+    [Header("Input Events")]
     [SerializeField] public UnityEvent trigEv;
-
 
     private void Awake()
     {
         /* CACHE SHIT */
 
-        /* Default */
-        curBay = 0;
+        /* Set Default */
+        curBay = weaponBay.cannon;
         gunneryMode = false;
+        cannonTimer = 0f;
+        mgunTimer = 0f;
+        missileTimer = 0f;
+        cannonRet.SetActive(false);
+        mgunRet.SetActive(false);
+        missileRet.SetActive(false);
 
         /* Input Action SetUp */
         trigActRef.action.performed += TrigAction;
     }
 
-    private void RotateBay()
-    {
-        //change to auto stage rotation
-        combatRing.Rotate(0, 5 * rotateSpd, 0); 
-    }
 
+    private void LateUpdate()
+    {
+        /* Update Console UI elements */
+        if(cannonTimer >= 0)
+            TMPcannonTimer.SetText("Cannon: READY FIRE");
+        else
+            TMPcannonTimer.SetText("Cannon: RELOADING: " + cannonTimer);
+
+        if (mgunTimer >= 0)
+            TMPmgunTimer.SetText("Machine Gun: READY FIRE");
+        else
+            TMPmgunTimer.SetText("Machine Gun: RELOADING " + mgunTimer);
+
+        if (missileTimer >= 0)
+            TMPmissileTimer.SetText("Missile: READY FIRE");
+        else
+            TMPmissileTimer.SetText("Missile: RELOADING " + missileTimer);
+    }
     private void TrigAction(InputAction.CallbackContext obj)
     {
         trigEv.Invoke();
+    }
+
+    private void RotateBay(int bayNo)
+    {
+        combatRing.Rotate(0, 5 * rotateSpd, 0);
+    }
+
+    public void GunnaryActive()
+    {
+        if (curBay == weaponBay.cannon)
+        {
+            cannonRet.SetActive(true);
+            mgunRet.SetActive(false);
+            missileRet.SetActive(false);
+        }
+        else if (curBay == weaponBay.mgun)
+        {
+            cannonRet.SetActive(false);
+            mgunRet.SetActive(true);
+            missileRet.SetActive(false);
+        }
+        else if (curBay == weaponBay.cannon)
+        {
+            cannonRet.SetActive(false);
+            mgunRet.SetActive(false);
+            missileRet.SetActive(true);
+        }
+        else return;
     }
 
     private void Aim()
     {
         if (gunneryMode)
         {
-            //Vector3 aimDir = aimRet.position - rightHandCon.position;
-            //RAYCAST DIRECTION
-            //Ray ray = Physics.Raycast(rightHandCon.position, )
+            if (curBay == weaponBay.cannon)
+            {
+
+            }
+            else if (curBay == weaponBay.mgun)
+            {
+
+            }
+            else if (curBay == weaponBay.cannon)
+            {
+
+            }
+            else return;
         }
         else return;
     }
@@ -61,5 +139,13 @@ public class ssManager : MonoBehaviour
     private void Fire()
     {
         //for loop to fire through current list of weapons
+
     }
+}
+
+public enum weaponBay
+{
+    cannon,
+    mgun,
+    missile
 }
